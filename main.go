592288b0913d9be8bdec2f92f1da759b4240c508
Kayrat Kaipov <kaipovich24@gmail.com>
2025-03-21 16:33:03 +0600

    Fetch

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -37,9 +38,12 @@ func main() {
 		log.Fatal("Can't connecto to db", err)
 	}
 
+	db := database.New(conn)
 	apiCfg := apiConfig{
-		DB: database.New(conn),
+		DB: db,
 	}
+
+	go startScraping(db, 10, time.Minute)
 
 	router := chi.NewRouter()
 
